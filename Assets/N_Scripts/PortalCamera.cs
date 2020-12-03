a77b95cc373b0f13b42a36a4029c89b71f06f213
NelsonSalvador <nelsonsalvadorzx@gmail.com>
2020-12-03 23:50:29 +0000

    Growth Tunnel programing

---
+++ Assets/N_Scripts/PortalCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCamera : MonoBehaviour
+{
+    public Transform playerCamera;
+    public Transform portal;
+    public Transform otherPortal;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerOffset = playerCamera.position - portal.position;
+        transform.position = otherPortal.position + playerOffset;
+
+        float angularDiff = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+
+        Quaternion portalRotatDiff = Quaternion.AngleAxis(angularDiff, Vector3.up);
+        Vector3 newCamDirection = portalRotatDiff * playerCamera.forward;
+
+        transform.rotation = Quaternion.LookRotation(newCamDirection, Vector3.up);
+    }
+}
---
+++ Assets/N_Scripts/PortalTeleporter.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTeleporter : MonoBehaviour
+{
+    public Transform player;
+    public Transform teleportPoint;
+
+    public Transform renderPlane;
+
+    private bool playerIsOverlapping = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Debug.Log(teleportPoint.position);
+        if (playerIsOverlapping)
+        {
+            Vector3 portalToPlayer = player.position - transform.position;
+
+            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+
+            if(dotProduct < 0f)
+            {
+                CharacterController cc = player.GetComponent<CharacterController>();
+                cc.enabled = false;
+                
+                float rotationDiff = -Quaternion.Angle(transform.rotation, teleportPoint.rotation);
+                //rotationDiff += 180;
+                player.Rotate(Vector3.up, rotationDiff);
+
+                Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+
+                Vector3 portalOffsets = new Vector3 (transform.position.x - renderPlane.position.x, 0, 0);
+                player.position = teleportPoint.position + positionOffset + portalOffsets;
+
+                cc.enabled = true;
+                playerIsOverlapping = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            playerIsOverlapping = true;
+        }
+    }
+}
