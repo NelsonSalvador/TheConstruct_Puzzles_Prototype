c68b8cd2c08eff61a1c6dccdf80b73b1fcbc90a3
NelsonSalvador <nelsonsalvadorzx@gmail.com>
2020-12-04 18:50:43 +0000

    Growth Tunnel fixes and add player growth

--- Assets/N_Scripts/PortalTextureSetUp.cs
+++ Assets/N_Scripts/PortalTextureSetUp.cs
@@ -6,6 +6,8 @@ public class PortalTextureSetUp : MonoBehaviour
 {
     public Camera cameraB;
     public Material cameraMatB;
+    public Camera cameraA;
+    public Material cameraMatA;
 
     // Start is called before the first frame update
     void Start()
@@ -13,9 +15,13 @@ public class PortalTextureSetUp : MonoBehaviour
         if(cameraB.targetTexture != null)
         {
             cameraB.targetTexture.Release();
+            cameraA.targetTexture.Release();
         }
         cameraB.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
         cameraMatB.mainTexture = cameraB.targetTexture;
+
+        cameraA.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraMatA.mainTexture = cameraA.targetTexture;
     }
 
     // Update is called once per frame
