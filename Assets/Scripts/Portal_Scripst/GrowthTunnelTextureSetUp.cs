430ce0ab0e3b7cac4f78727d2395873f7d53a8f8
NelsonSalvador <nelsonsalvadorzx@gmail.com>
2020-12-14 20:56:57 +0000

    Fix bugs and interactions for portal puzzles

---
+++ Assets/Scripts/Portal_Scripst/GrowthTunnelTextureSetUp.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowthTunnelTextureSetUp : MonoBehaviour
+{
+    public Camera cameraB;
+    public Material cameraMatB;
+    public Camera cameraA;
+    public Material cameraMatA;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(cameraB.targetTexture != null)
+        {
+            cameraB.targetTexture.Release();
+        }
+        if (cameraA.targetTexture != null)
+        {
+            cameraA.targetTexture.Release();
+        }
+        cameraB.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraMatB.mainTexture = cameraB.targetTexture;
+
+        cameraA.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraMatA.mainTexture = cameraA.targetTexture;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
