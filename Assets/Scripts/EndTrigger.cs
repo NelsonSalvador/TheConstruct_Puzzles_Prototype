ea7b072be60af02f9c86c5839cb43218e097d855
NelsonSalvador <nelsonsalvadorzx@gmail.com>
2020-12-15 18:12:22 +0000

    Add pause menu and end trigers

---
+++ Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - SceneManager.GetActiveScene().buildIndex);
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+            
+    }
+}
