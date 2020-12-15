ff5b0e84fffff3936167f6d29678c20222356048
NelsonSalvador <nelsonsalvadorzx@gmail.com>
2020-12-15 18:54:46 +0000

    Floor deform Script

---
+++ Assets/Scripts/Floor_deform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor_deform : MonoBehaviour
+{
+    public GameObject Player;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance;
+        distance = Vector3.Distance(Player.transform.position, transform.position);
+        if (distance > 5.0f)
+        {
+            transform.position = new Vector3(transform.position.x, (distance-5)/3 , transform.position.z);
+            if(distance < 15.0f)
+            {
+                transform.localScale = new Vector3((15 - distance) * 0.1f, (15 - distance) * 0.1f, (15 - distance) * 0.1f);
+            }
+            else
+            {
+                transform.localScale = new Vector3(0, 0, 0);
+            }
+        }
+        
+    }
+}
