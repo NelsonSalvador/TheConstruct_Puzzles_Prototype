c3987037d3437a0f943fc295ead237b39f183af0
João Borges <joaonunos.borges@gmail.com>
2021-01-29 17:04:13 +0000

    Added commentary

--- Assets/Scripts/Interaction_Scripts/CanvasMng.cs
+++ Assets/Scripts/Interaction_Scripts/CanvasMng.cs
@@ -1,24 +1,31 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.UI;
-
+/// <summary>
+/// Script for Canvas Manager that is responsible for displaying and changing
+/// the interaction text and the inventory UI
+/// </summary>
 public class CanvasMng : MonoBehaviour
 {
     public GameObject   interactionPanel;
     public Text         interactionText;
     public RawImage[]   inventoryIcons;
 
+    // Start is called before the first frame update
     void Start()
     {
         HideInteractionPanel();
     }
 
+    /// <summary>
+    /// Hides the panel with interaction text
+    /// </summary>
     public void HideInteractionPanel()
     {
         interactionPanel.SetActive(false);
     }
 
     /// <summary>
-    /// Shows interaction pannel.
+    /// Sets the text in the interaction panel and shows it.
     /// </summary>
     /// <param name="textMessage">Interaction text.</param>
     public void ShowInteractionPanel(string textMessage)
@@ -28,12 +35,11 @@ public class CanvasMng : MonoBehaviour
         interactionPanel.SetActive(true);
     }
 
-    //Cria os icons para objetos no inventário
 
     /// <summary>
-    /// Sets the inventory Icons.
+    /// Sets the inventory icon for a specific slot in inventory.
     /// </summary>
-    /// <param name="i"></param>
+    /// <param name="i">Slot in inventory</param>
     /// <param name="icon">Icon Texture</param>
     public void SetInventoryIcon(int i, Texture icon)
     {
@@ -41,6 +47,11 @@ public class CanvasMng : MonoBehaviour
         inventoryIcons[i].color = Color.clear;
     }
 
+    /// <summary>
+    /// Sets the selected inventory icon to be highlighted
+    /// </summary>
+    /// <param name="n">Selected icon</param>
+    /// <param name="inventoryCount">number of inventory slots in use</param>
     public void SetSelectedIcon(int n, int inventoryCount)
     {
         for (int i = 0; i < inventoryCount; ++i)
@@ -49,7 +60,9 @@ public class CanvasMng : MonoBehaviour
     }
 
 
-    //Apaga os icons para objetos no inventário (para não ficar lá imagem quando se apaga um objeto)
+    /// <summary>
+    /// Removes the icons and color from the inventory slots
+    /// </summary>
     public void ClearInventoryIcons()
     {
         for (int i = 0; i < inventoryIcons.Length; ++i)
